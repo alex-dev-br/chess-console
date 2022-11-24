eba86e0f84069b83c32d9c9e1cf842a17484d2cf
Alexandre Andrade <alex_dev@outlook.com.br>
2022-11-24 05:27:41 -0300

    Creating the Position class.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using board;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Position P;
+        P = new Position(3, 4);
+
+        Console.WriteLine("Position: " + P);
+        Console.WriteLine();
+    }
+}
